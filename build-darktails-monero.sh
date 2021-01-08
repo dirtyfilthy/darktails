6774d49e161558af6198eacb601493a4149810ab
alhazred <alhazred@debian-build.local>
2021-01-09 05:52:06 +1100

    add monero deb

--- build-darktails-monero.sh
+++ build-darktails-monero.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 SCRIPTPATH=$(cd $(dirname $0) && pwd)
 MODULEPATH=$SCRIPTPATH/submodules/darktails-monero
-BUILDPATH=$MODULEPATH/build/release/bin
-SKELPATH=$MODULEPATH/dist/deb/skel
-DESTPATH=$SCRIPTPATH/config/chroot_local-includes
-echo "BUILD PATH : $BUILDPATH"
-echo "SKEL PATH  : $SKELPATH"
-echo "DEST PATH  : $DESTPATH"
-git submodule update --init --recursive
-git submodule update --remote --recursive
-(
-	cd $MODULEPATH
-	./build-darktails.sh
-)
-cp -r $SKELPATH/*  $DESTPATH
-cp -r $BUILDPATH/* $DESTPATH/usr/local/bin
-
+( cd $MODULEPATH && ./build-darktails-deb.sh )
+DEBPATH=$MODULEPATH/dist/deb/build/darktails-monero-gui.deb
+cp $DEBPATH config/chroot_local-packages/
